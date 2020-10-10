fc99d4155f08b8904fa92ead7cf2b4e6bb886090
elicavalheiro <elimara.ac@gmail.com>
2020-10-10 02:42:17 -0300

    :sparkles: list and update users functions

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,8 +1,14 @@
-import { Request, Response } from 'express';
+import { query, Request, Response } from 'express';
 
 import db from '../database/connection';
 
 export default class UsersController {
+  async list(request: Request, response: Response){
+    const users = await db('users');
+
+    return response.json(users);
+  }
+
   async create(request: Request, response: Response){
     const {
       name,
@@ -18,6 +24,24 @@ export default class UsersController {
       password
     });
 
-    return response.send({ message: 'Usuário criado.' });
+    return response.json({ message: 'Usuário criado.' });
+  }
+
+  async update(request: Request, response: Response){
+    const userId = request.params.id;
+    const payload = request.body;
+
+    await db('users').where('id', userId).update({
+      name: payload.name,
+      email: payload.email,
+      login: payload.login,
+      password: payload.password,
+      avatar: payload.avatar
+    });
+
+    return response.json({ 
+      message: 'Informações atualizadas',
+      data: payload
+    });
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,8 @@ const router = Router();
 // Controllers
 const users_controller = new UsersController();
 
+router.get('/users', users_controller.list);
 router.post('/users', users_controller.create);
+router.put('/users/:id', users_controller.update);
 
 export default router;

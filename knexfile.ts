6a4188b7f1bbc93dfa07b0a21d333c081110c6bf
elicavalheiro <elimara.ac@gmail.com>
2021-02-11 21:56:37 -0300

    :art: adjuts project

--- knexfile.ts
+++ knexfile.ts
@@ -1,13 +1,25 @@
 import path from 'path';
-require('dotenv').config();
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+});
 
-module.exports = {
-  client: 'pg',
-  connection: {
+const connection = () => {
+  if(process.env.NODE_ENV === 'test'){
+    return {
+      filename: path.resolve(__dirname, '__tests__', 'database.sqlite')
+    }
+  }
+
+  return {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE
-  },
+  }
+}
+
+module.exports = {
+  client: process.env.DB_CLIENT || 'pg',
+  connection,
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
   },
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,13 +1,26 @@
 import knex from 'knex';
-require('dotenv').config();
+import path from 'path';
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+});
 
-const db = knex({
-  client: 'pg',
-  connection: {
+const connection = () => {
+  if(process.env.NODE_ENV === 'test'){
+    return {
+      filename: path.resolve(__dirname, '__tests__', 'database.sqlite')
+    }
+  }
+
+  return {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE
-  },
+  }
+}
+
+const db = knex({
+  client: process.env.DB_CLIENT || 'pg',
+  connection,
   useNullAsDefault: true,
 });
 

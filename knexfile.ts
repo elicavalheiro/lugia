147dbd258ecfaed05f66b173818ce168c13db555
elicavalheiro <elimara.ac@gmail.com>
2021-02-17 23:15:23 -0300

    :card_file_box: database config

--- knexfile.ts
+++ knexfile.ts
@@ -13,6 +13,8 @@ const connection = () => {
   return {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    port: process.env.PORT,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 require('dotenv').config();
-import path from 'path';
 import app from './app';
 
 const port = process.env.APP_PORT || 3333;
 
-console.log(path.resolve(__dirname, '..' , '__tests__', 'database.sqlite'))
-
 app.listen(port, () => {
-  console.log(`--- Server running at port ${port} ---`)
+  console.log(`--- Server running at port ${port} ---`);
 });
